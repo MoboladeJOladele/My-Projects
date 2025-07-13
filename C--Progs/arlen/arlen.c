6ab6a0e551ea6d93b3ab90b0e2c24c99c31adf40
MoboladeJOladele <moboladej.oladele@gmail.com>
2025-07-14 00:10:03 +0100

    Update arlen.c

--- C--Progs/arlen/arlen.c
+++ C--Progs/arlen/arlen.c
@@ -1,80 +1,48 @@
 #include <stdio.h>
-#include <string.h>
 
-// Int functions stacked alphabetically
-int char_len(char* array, int size);
-int double_len(double* array, int size);
-int float_len(float* array, int size);
-int int_len(int* array, int size);
-int long_len(long* array, int size);
-int string_len(char** array, int size);
+#define arlen_others(type, name) \
+    int name(type array, int size) { \
+        return size / sizeof(array[0]); \
+    }
 
+// Other arlen functions
+arlen_others(int*, arlen_ints)
+arlen_others(long*, arlen_longs)
+arlen_others(float*, arlen_floats)
+arlen_others(double*, arlen_doubles)
+arlen_others(char**, arlen_string_set)
 
+// String (char*) arlen function
+int arlen_string(char* s, int size) {
+    int length = 0;
+    for (int i = 0; s[i] != '\0'; i++)
+    {
+        length++;
+    }
+
+    return length;
+}
+
+// _Generic Function Selector
 #define arlen(array, size) _Generic((array), \
-        char*:   char_len,    \
-        char**:  string_len,  \
-        int*:    int_len,     \
-        long*:   long_len,    \
-        float*:  float_len,   \
-        double*: double_len   \
+    int*: arlen_ints, \
+    long*: arlen_longs, \
+    float*: arlen_floats, \
+    double*: arlen_doubles, \
+    char*: arlen_string, \
+    char**: arlen_string_set \
 )(array, size)
 
 int main(void)
 {
-    // Create an array of intgers
-    int numbers[] = {7, 6, 8, 18, 23};
+    char *string = "hello, world!";
+    char *string_set[] = {"yes", "no", "maybe"};
+    int num[] = {12, 13, 26, 0, 1, 1};
 
-    // Create a string (an array of chars)
-    char name[] = {"Joseph"};
+    printf("Length of string: %i\n", arlen(string, sizeof(string)));
+    printf("Length of string set: %i\n", arlen(string_set, sizeof(string_set)));
+    printf("Length of number array: %i\n", arlen(num, sizeof(num)));
 
-    // Create an array of strings (char *)
-    char *org[] = {"Github", "Youtube", "Google", "Meta"};
-
-    // Return the size of the first array
-    printf("Int: %i\n", arlen(numbers, sizeof(numbers)));
-
-    // Return the size of the second array
-    printf("Char: %i\n", arlen(name, sizeof(name)));
-
-    // Return the size of the third array
-    printf("String: %i\n", arlen(org, sizeof(org)));
-
-    // Return the size of an element (or sub-array) within the parent array
-    printf("El: %lu\n", strlen(org[2]));
-}
-
-int char_len(char* array, int size)
-{
-    int len = size / sizeof(array[0]);
-    return len;
-}
-
-int int_len(int* array, int size)
-{
-    int len = size / sizeof(array[0]);
-    return len;
-}
-
-int long_len(long* array, int size)
-{
-    int len = size / sizeof(array[0]);
-    return len;
-}
-
-int float_len(float* array, int size)
-{
-    int len = size / sizeof(array[0]);
-    return len;
-}
-
-int double_len(double* array, int size)
-{
-    int len = size / sizeof(array[0]);
-    return len;
-}
-
-int string_len(char** array, int size)
-{
-    int len = size / sizeof(array[0]);
-    return len;
+    // Length of an element in the string set
+    printf("\nLength of Element: %i\n", arlen(string_set[2], sizeof(string_set[2])));
 }
